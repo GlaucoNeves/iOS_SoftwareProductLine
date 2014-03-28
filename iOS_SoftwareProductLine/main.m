e1f0b3d77c9dc13ab1eb9957237bd0b9064e87b7
Glauco Neves <glauco@moblee.net>
2014-03-27 22:29:33 -0300

    First commit! Configuring the project.

---
+++ iOS_SoftwareProductLine/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  iOS_SoftwareProductLine
+//
+//  Created by mobLee on 3/27/14.
+//  Copyright (c) 2014 GlaucoNeves. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+
+}
---
+++ iOS_SoftwareProductLineTests/iOS_SoftwareProductLineTests.m
@@ -0,0 +1,34 @@
+//
+//  iOS_SoftwareProductLineTests.m
+//  iOS_SoftwareProductLineTests
+//
+//  Created by mobLee on 3/27/14.
+//  Copyright (c) 2014 GlaucoNeves. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iOS_SoftwareProductLineTests : XCTestCase
+
+@end
+
+@implementation iOS_SoftwareProductLineTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
